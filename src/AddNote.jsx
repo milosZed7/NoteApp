4f4db63aea9a4484273b4f78bbe37bb686fd8a8c
Milos Zjajic <miloszjajic@gmail.com>
2018-02-15 23:50:40 +0100

    implement note adding and error handling

--- src/AddNote.jsx
+++ src/AddNote.jsx
@@ -2,19 +2,43 @@ import React from 'react';
 
 class AddNote extends React.Component {
     render() {
-        const btnClickedClass = this.props.saveBtnOn ? 'off-on-btn-clicked' : '';
-        const ballTranslateClass = this.props.saveBtnOn ? 'translate-ball' : '';
-        const opacitySaveBtnClass = this.props.saveBtnOn ? 'add-note-btn-opacity' : '';
-        const translateNoteInputClass = this.props.saveBtnOn ? 'note-input-translate' : '';
+        const btnClickedClass = !this.props.saveBtnOn ? 'off-on-btn-clicked' : '';
+        const ballTranslateClass = !this.props.saveBtnOn ? 'translate-ball' : '';
+        const opacitySaveBtnClass = !this.props.saveBtnOn ? 'add-note-btn-opacity' : '';
+        const translateNoteInputClass = !this.props.saveBtnOn ? 'note-input-translate' : '';
         return (
             <div className={`add-note`}>
-                <textarea className={`note-input ${translateNoteInputClass}`} placeholder="Add a note..."></textarea>
-                <button className={`add-note-btn ${opacitySaveBtnClass}`}>Save note</button>
+                <input
+                    type="text"
+                    className="note-input-title"
+                    placeholder="Note title"
+                    maxLength="50"
+                    value={this.props.note.title}
+                    onChange={this.props.noteTitleChange}
+                />
+                <textarea
+                    className={`note-input-text ${translateNoteInputClass}`}
+                    placeholder="Add a note..."
+                    value={this.props.note.text}
+                    onKeyUp={this.props.addNote}
+                    onChange={this.props.noteTextChange}
+                />
+                <button className={`add-note-btn ${opacitySaveBtnClass}`} onClick={this.props.addNote}>
+                    Save note
+                </button>
                 <div className="save-type">
-                    <div className="press-enter">Press <span>enter</span> to save</div>
-                    <div className={`off-on-btn ${btnClickedClass}`} onClick={this.props.showSaveBtn}>
-                        <div className={`ball ${ballTranslateClass}`}></div>
+                    <div className="press-enter">
+                        Press <span>enter</span> to save
                     </div>
+                    <div className={`off-on-btn ${btnClickedClass}`} onClick={this.props.showSaveBtn}>
+                        <div className={`ball ${ballTranslateClass}`} />
+                    </div>
+                </div>
+                <div className={`note-title-error ${this.props.noteTitleError ? 'note-title-error-show' : ''}`}>
+                    Title empty
+                </div>
+                <div className={`note-text-error ${this.props.noteTextError ? 'note-text-error-show' : ''}`}>
+                    Note content empty
                 </div>
             </div>
         );
--- src/Notes.jsx
+++ src/Notes.jsx
@@ -2,32 +2,102 @@ import React from 'react';
 import NoteList from './NoteList';
 import AddNote from './AddNote';
 class Notes extends React.Component {
-
     state = {
         notes: [
             { id: 1, text: 'first note', date: '14.02.2018.', title: 'Note title1' },
-            { id: 2, text: 'second note', date: '14.02.2018.', title: 'Note title2' },
-            { id: 3, text: 'third note', date: '14.02.2018.', title: 'Note title3' },
-            { id: 4, text: 'third note', date: '14.02.2018.', title: 'Note title3' },
-            { id: 5, text: 'third note', date: '14.02.2018.', title: 'Note title3' },
-            
+            { id: 2, text: 'second note', date: '14.02.2018.', title: 'Note title2' }
         ],
-        saveBtnOn: false,
+        saveBtnOn: true,
+        noteTitleError: false,
+        noteTextError: false,
+        newNote: {
+            title: '',
+            text: '',
+            date: '',
+            id: ''
+        }
+    };
+    resetNote = () => {
+        return {
+            title: '',
+            text: '',
+            date: '',
+            id: ''
+        };
     };
 
-    showSaveBtn = (evt) => {
+    showSaveBtn = evt => {
         if (evt.button !== 0) return;
         this.setState({
             saveBtnOn: !this.state.saveBtnOn
         });
-    }
+    };
+
+    noteTextChange = evt => {
+        this.setState({
+            newNote: Object.assign({}, this.state.newNote, { text: evt.target.value })
+        });
+    };
+    noteTitleChange = evt => {
+        this.setState({
+            newNote: Object.assign({}, this.state.newNote, { title: evt.target.value })
+        });
+    };
+    addNote = evt => {
+        if ((this.state.saveBtnOn && evt.keyCode === 13) || evt.button === 0) {
+            const newNote = Object.assign({}, this.state.newNote);
+
+            if (!newNote.title.trim() || !newNote.text.trim()) {
+                this.setState({
+                    newNote: this.resetNote(),
+                    noteTextError: !newNote.text.trim(),
+                    noteTitleError: !newNote.title.trim()
+                });
+                return;
+            }
+
+            newNote.date = getDate();
+            newNote.id = genId.bind(this)();
+            this.setState({
+                newNote: this.resetNote(),
+                notes: this.state.notes.concat(newNote),
+                noteTextError: false,
+                noteTitleError: false
+            });
+        }
+
+        function genId() {
+            let number = Math.floor(Math.random() * 900);
+            if (this.state.notes.some(note => note.id === number)) {
+                number = genId.bind(this)();
+            }
+            return number;
+        }
+
+        function getDate() {
+            const date = new Date();
+            const day = date.getDate();
+            const mounth = date.getMonth() + 1;
+            const year = date.getFullYear();
+            return `${day < 10 ? '0' + day : day}.${mounth < 10 ? '0' + mounth : mounth}.${year}.`;
+        }
+    };
 
     render() {
         return (
             <div className="note-component">
                 <div className="note-wrapper">
                     <NoteList notes={this.state.notes} />
-                    <AddNote showSaveBtn={this.showSaveBtn} saveBtnOn={this.state.saveBtnOn} />
+                    <AddNote
+                        showSaveBtn={this.showSaveBtn}
+                        saveBtnOn={this.state.saveBtnOn}
+                        note={this.state.newNote}
+                        addNote={this.addNote}
+                        noteTitleChange={this.noteTitleChange}
+                        noteTextChange={this.noteTextChange}
+                        noteTextError={this.state.noteTextError}
+                        noteTitleError={this.state.noteTitleError}
+                    />
                 </div>
             </div>
         );
