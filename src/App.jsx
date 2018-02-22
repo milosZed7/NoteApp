dfc8508e1eefd5ab711687617c79836851775f2c
Milos Zjajic <miloszjajic@gmail.com>
2018-02-22 20:14:17 +0100

    added registration an login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import './login.css';
+import './register.css';
 import Notes from './Notes';
+import Login from './Login';
+import Registration from './Registration';
 
-class App extends Component {
-  render() {
+const fourOFour = () => <h1>404</h1>;
+
+const App = () => {
     return (
-      <div className="App">
-        <Notes />
-      </div>
+        <BrowserRouter>
+            <div className="App">
+                <Switch>
+                    <Route exact path="/" component={Notes} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Registration} />
+                    <Redirect from="/home" to="/" />
+                    <Redirect from="/notes" to="/" />
+                    <Route component={fourOFour} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
-  }
-}
+};
 
 export default App;
